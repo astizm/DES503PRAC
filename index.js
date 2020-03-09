cce5652d6f62100ee2de7a1431fe4adf586f8748
astizm <asti-maera@live.nmit.ac.nz>
2020-03-10 09:12:26 +1300

    Assignment Op

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-console.log(10 + 5 * 3) //BODMAS
-
-console.log(1 + 2, 3 + 4) // Comma operator allows concurrant expressions
-
-console.log ("Comma Operator in a Variable");
-
-let a = (1 + 2, 3+ 4);
-console.log (a) //First expression is thrown in the garbage
+let x = 10; // = is the assignment operator
+alert(x);
+console.log (x)
