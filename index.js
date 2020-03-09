a4227bd50816842266d84c375273715ae7e20832
astizm <asti-maera@live.nmit.ac.nz>
2020-03-10 09:49:30 +1300

    comparison of logical returns

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let x = 10 + 5 * 2 // = has lower precedence than +-*/
-console.log (x)
+//Boolean
+console.log(10 == 5) //false (10 != 5)
+console.log( 10!=10) //F (10 is = 10)
+console.log (10>2) //T (10 is > 2)
+console.log(10<5) //F (10 is > 5)
+console.log(10<=5) //F (10>=5)
+console.log(10>=5); //T (10>5)
