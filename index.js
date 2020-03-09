e3e3f56d2193be8a2106c3c875a06148e3b33a3c
astizm <asti-maera@live.nmit.ac.nz>
2020-03-09 14:11:14 +1300

    final example

--- index.js
+++ index.js
@@ -29,3 +29,15 @@ console.log(y)
 console.log(--y)
 console.log(y)
 console.log(--y)
+
+console.log("Pre-Increment")
+let a=1
+console.log(a);
+console.log(10 * ++a)
+console.log(a);
+
+console.log("Post-Increment")
+let b=1
+console.log(b);
+console.log(10 * b++)
+console.log(b);
