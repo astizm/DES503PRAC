c99319ef1f80d0287814abfc0444437dd11cd86b
astizm <asti-maera@live.nmit.ac.nz>
2020-03-10 10:26:13 +1300

    Comparison of different types

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ console.log (10>2) //T (10 is > 2)
 console.log(10<5) //F (10 is > 5)
 console.log(10<=5) //F (10>=5)
 console.log(10>=5); //T (10>5)
+
+console.log(10 == "5")//F (!=10)
+console.log(10 === "10"); // F not identical (switch to == or for T)
+console.log(10 !=  "5"); //T
+console.log(10 != "10");//F switch to == for T 
+console.log(10 < "5")// F <less than switch to > for T
+console.log(10 < "10")// F
+console.log(10 > "10" )// F
+console.log(10 <= "5")//False (Less than or =)
+console.log(10 <= 10)// True
+console.log(10 >= "5")//True, Greater than or = 
+console.log(10 >= "10")//True (= to)
