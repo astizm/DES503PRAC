b914e398adbf21130cf41e29c5eb99ab635d72d4
astizm <asti-maera@live.nmit.ac.nz>
2020-03-16 14:04:19 +1300

    break omissions

--- index.js
+++ index.js
@@ -1,50 +1,27 @@
-//compare objects and arrays with ===/==
-const a = {
-    isHavingFun: true
-}
-const b = a
-console.log(a === b) //f
-console.log(a == b)//f
-console.log(a !== b)//f
-console.log(a) //{ isHavingFun: true}
-console.log(b) //{ isHavingFun: true}
-//truthy vs falsey
-let green = 2;
-let red = 5;
-let orange = 4;
-
-//switch statements
-const person = 'Mart'
-switch (person) {
-    case 'Jane' :
-        console.log('Hi Jane')
-        break;
-        case "Joe":
-            console.log('Hi Joe')
-            break;
-            case 'Mart':
-                console.log('Hi Mart')
+//break omission from switch statements
+const yo = 0
+let output = "";
+switch (yo) {
+    case 0:
+        output += 'Yo,';
+        case 1:
+            output += 'Mama'
+            output += 'So'
+            case 2:
+                output += 'Old'
+                console.log(output); //Yo,MamaSoOld
                 break;
-                
+                case 3:
+                    output += 'You'
+                    case 4:
+                        output += 'Know'
+                        break;
+                        case 5:
+                            output += '!';
+                            console.log(output);
+                            break;
+                            default:
+                                console.log('Pick a number 5 or less!');
+
+
 }
-//multi-criteria case
-const foo = 12
-switch (foo) {
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-    console.log('I like this number');
-    break;
-  case -1:
-  case -2:
-    console.log('I love this number');
-    case 12:
-    case 13:
-    case 15:
-        console.log('Numbers are numerical')
-    break;
-  default:
-    console.log('I don\'t like this number.');
-}
-non-number
